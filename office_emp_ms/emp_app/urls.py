7c35489b56ff2ca94b425c7cfb8e4b57d4202ff1
wolfsbane-14 <jatinsinghfourteen@gmail.com>
2024-09-06 14:31:02 +0530

    new changes

--- office_emp_ms/emp_app/urls.py
+++ office_emp_ms/emp_app/urls.py
@@ -19,4 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path("",views.index, name = 'index'),
+    path("all_emp",views.all_emp, name = 'all_emp'),
+    path("add_emp",views.add_emp, name = 'add_emp'),
+    path("remove_emp",views.remove_emp, name = 'remove_emp'),
+    path("filter_emp",views.filter_emp, name = 'filter_emp'),
 ]
