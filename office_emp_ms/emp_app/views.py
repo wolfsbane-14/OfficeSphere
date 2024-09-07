2a14109929730cc17f4da46218749e4c3741e843
wolfsbane-14 <jatinsinghfourteen@gmail.com>
2024-09-07 20:24:18 +0530

    working on project

--- office_emp_ms/emp_app/views.py
+++ office_emp_ms/emp_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Employee, Role, Department
+from datetime import datetime
 
 # Create your views here.
 def index(request):
@@ -14,7 +15,19 @@ def all_emp(request):
     return render(request,'all_emp.html',context)
 
 def add_emp(request):
-    return render(request,'add_emp.html')
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST["last_name"]
+        salary = int(request.POST["salary"])
+        bonus = int(request.POST["bonus"])
+        phone = int(request.POST["phone"])
+        dept = int(request.POST["dept"])
+        role = int(request.POST["role"])
+        new_emp = Employee(first_name = first_name, last_name = last_name,salary = salary,bonus = bonus,phone = phone, dept_id = dept, role_id = role, hire_date = datetime.now)
+        new_emp.save()
+    else:
+        return render(request,"add_emp.html")
+
 
 def remove_emp(request):
     return render(request,'remove_emp.html')
