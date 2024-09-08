f7385d197dc352c3f125236e0b7a244b12e49d56
wolfsbane-14 <jatinsinghfourteen@gmail.com>
2024-09-08 15:00:50 +0530

    final changes

--- office_emp_ms/emp_app/views.py
+++ office_emp_ms/emp_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse
 from .models import Employee, Role, Department
 from datetime import datetime
+from django.db.models import Q
 
 # Create your views here.
 def index(request):
@@ -47,5 +48,24 @@ def remove_emp(request,emp_id = 0):
     return render(request,'remove_emp.html',context)
 
 def filter_emp(request):
-    if  
-    return render(request,'filter_emp.html')
+    if request.method == "POST":
+        name = request.POST['name']
+        dept = request.POST["dept"]
+        role = request.POST["role"]
+        emps = Employee.objects.all()
+        if name:
+            emps = emps.filter(Q(first_name__icontains = name) | Q(last_name__icontains= name))
+        if dept:
+            emps = emps.filter(dept__name__icontains = dept)
+        if role:
+            emps = emps.filter(role__name__icontains = role)
+
+        context = {
+            'emps': emps
+        }
+
+        return render(request, 'all_emp.html',context)
+    elif request.method == "GET":
+        return render(request, 'filter_emp.html')
+    else:
+        return HttpResponse("An exception occured")
